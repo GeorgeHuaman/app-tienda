be7128dbf40d8b2e354ba7af7a53f54b9dc9ac8c
George Jimy <103298857+GeorgeHuaman@users.noreply.github.com>
2024-09-28 16:47:58 -0500

    Activar/DesactivarProps

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,100 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Manager : MonoBehaviour
 {
     public static Manager Instance;
     public Renderer selectedRenderer;
     public Material SelectedMaterial;
-    public List<MainMaterial> mainMaterials;
 
-    [SerializeField] private int currentMainMaterialIndex = 0;
-    [SerializeField] private int currentVariantIndex = 0;
-    //public Image mainTextures;
-    //public Image variantTextures;
+    public List<PropsGameObject> propsGameObjects;
     private void Awake()
     {
         Instance = this;
     }
-    private void Start()
-    {
-        //UpdateMainTextureUI();
-        //UpdateVariantTextureUI();
-    }
-    public void ChangeMainMaterial()
-    {
-        if (mainMaterials.Count > 0)
-        {
-            currentMainMaterialIndex = (currentMainMaterialIndex + 1) % mainMaterials.Count;
-            currentVariantIndex = 0;
-            //ApplyMainMaterial();
-            //UpdateMainTextureUI();
-            //UpdateVariantTextureUI();
-
-        }
-    }
-
-    public void ChangeVariantMaterial()
-    {
-        if (mainMaterials.Count > 0 && mainMaterials[currentMainMaterialIndex].variants.Count > 0)
-        {
-
-            currentVariantIndex = (currentVariantIndex + 1) % mainMaterials[currentMainMaterialIndex].variants.Count;
-            //ApplyVariantMaterial();
-            //UpdateVariantTextureUI();
-
-        }
-    }
-
-    public void ApplyMainMaterial()
-    {
-        if (selectedRenderer != null)
-        {
-            //selectedRenderer.material = mainMaterials[currentMainMaterialIndex].mainMaterial;\
-            selectedRenderer.material = SelectedMaterial;
-        }
-    }
 
     public void ApplyVariantMaterial()
     {
-        if (selectedRenderer != null && SelectedMaterial!= null/*&& mainMaterials[currentMainMaterialIndex].variants.Count > 0*/)
+        if (selectedRenderer != null && SelectedMaterial!= null)
         {
-            //selectedRenderer.material = mainMaterials[currentMainMaterialIndex].variants[currentVariantIndex];
             selectedRenderer.material = SelectedMaterial;
         }
     }
-    //private void UpdateMainTextureUI()
-    //{
-    //    Material currentMaterial = mainMaterials[currentMainMaterialIndex].mainMaterial;
-    //    Texture2D albedoTexture = (Texture2D)currentMaterial.GetTexture("_MainTex");
 
-    //    if (albedoTexture != null)
-    //    {
-    //        Sprite albedoSprite = Sprite.Create(albedoTexture, new Rect(0, 0, albedoTexture.width, albedoTexture.height), new Vector2(0.5f, 0.5f));
-    //        mainTextures.sprite = albedoSprite;
-    //    }
-    //}
-
-    //private void UpdateVariantTextureUI()
-    //{
-    //    Material currentVariant = mainMaterials[currentMainMaterialIndex].variants[currentVariantIndex];
-    //    Texture2D albedoTexture = (Texture2D)currentVariant.GetTexture("_MainTex");
-
-    //    if (albedoTexture != null)
-    //    {
-    //        Sprite albedoSprite = Sprite.Create(albedoTexture, new Rect(0, 0, albedoTexture.width, albedoTexture.height), new Vector2(0.5f, 0.5f));
-    //        variantTextures.sprite = albedoSprite;
-    //    }
-    //}
     public void SelectMaterial(Material material)
     {
         SelectedMaterial = material;
     }
-    [System.Serializable]
-    public class MainMaterial
+
+    public void EnabledDisableGameObject(string name)
     {
-        public Material mainMaterial;
-        public List<Material> variants = new List<Material>();
+        foreach (PropsGameObject props in propsGameObjects)
+        {
+            if (props.name == name)
+            {
+                props.propObject.SetActive(!props.propObject.activeSelf);
+            }
+        }
+    }
+    [System.Serializable]
+    public class PropsGameObject
+    {
+        public string name;
+        public GameObject propObject;
     }
 }
--- Assets/Scripts/OpenCloseUIMaterials.cs
+++ Assets/Scripts/OpenCloseUIMaterials.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class OpenCloseUIMaterials : MonoBehaviour
 {
-    public GameObject scrollview;
-
-    public void ActiveAndDesactive()
+    public Animator scrollViewTexturesAnimator;
+    public Animator scrollViewPropsAnimator;
+    public void ActiveAndDesactiveTexture()
     {
-        scrollview.SetActive(!scrollview.activeSelf);
+        if (scrollViewPropsAnimator.GetBool("svpIsOpen") == true)
+        {
+            scrollViewPropsAnimator.SetBool("svpIsOpen", false);
+        }
+
+        if (scrollViewTexturesAnimator.GetBool("svtIsOpen") == true)
+        {
+            scrollViewTexturesAnimator.SetBool("svtIsOpen", false);
+            Manager.Instance.SelectedMaterial = null;
+        }
+        else
+            scrollViewTexturesAnimator.SetBool("svtIsOpen", true);
+        
+    }
+    public void ActiveAndDesactiveProps()
+    {
+        if (scrollViewTexturesAnimator.GetBool("svtIsOpen") == true)
+        {
+            scrollViewTexturesAnimator.SetBool("svtIsOpen", false);
+        }
+
+        if (scrollViewPropsAnimator.GetBool("svpIsOpen") == true)
+            scrollViewPropsAnimator.SetBool("svpIsOpen", false);
+        else
+            scrollViewPropsAnimator.SetBool("svpIsOpen", true);
+        
     }
 }
